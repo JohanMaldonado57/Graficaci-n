1c33a3637da8f6a12c56e1269489fbf94811e26d
JohanMaldonado57 <johanmaldonado57@aragon.unam.mx>
2024-04-27 21:00:40 -0600

    Movimiento y Tilemap iniciales
    Se crea TileMap de piso nieve y se crea nivel de pruebas
    se crea Jugador con su respectivo Script que le permite moverse a la izquierda y derecha asi como salto aun sin animaciones
    se crea tambien la colision con el piso

---
+++ Proyecto6/Assets/Scripts/ColisionSuelo.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColisionSuelo : MonoBehaviour
+{
+    public static bool estaEnSuelo = true;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "suelo")
+        {
+            estaEnSuelo = true;
+        }
+    }
+
+}
